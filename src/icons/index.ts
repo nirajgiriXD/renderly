7c278c24fa11443fae2c2d03050ab3eac8c9496a
nirajgirixd <nirajgiri9860@gmail.com>
2026-01-15 22:39:43 +0545

    feat: add social media icons in SVG format

---
+++ src/icons/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Export all icons from this directory.
+ * @see https://www.svgrepo.com/
+ */
+
+export { default as Discord } from "./discord.svg";
+export { default as Facebook } from "./facebook.svg";
+export { default as LinkedIn } from "./linkedin.svg";
+export { default as Instagram } from "./instagram.svg";
+export { default as Messenger } from "./messenger.svg";
+export { default as Reddit } from "./reddit.svg";
+export { default as Signal } from "./signal.svg";
+export { default as Slack } from "./slack.svg";
+export { default as Snapchat } from "./snapchat.svg";
+export { default as Teams } from "./teams.svg";
+export { default as Telegram } from "./telegram.svg";
+export { default as Tiktok } from "./tiktok.svg";
+export { default as Twitch } from "./twitch.svg";
+export { default as Twitter } from "./twitter.svg";
+export { default as Viber } from "./viber.svg";
+export { default as WhatsApp } from "./whatsapp.svg";
+export { default as YouTube } from "./youtube.svg";
